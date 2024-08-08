926366ebdd95b60dbdba831cd4d9b6125b1e9e89
Ricardo S. Ferreira <ricardo.silva_ferreira@hotmail.com>
2024-08-08 17:05:09 -0300

    incrementando Login

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,33 @@
 import React from 'react';
-import Home from './pages/home/Home';
+import './App.css';
+
 import Navbar from './components/NavBar/NavBar';
 import Footer from './components/Footer/Footer';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Login from './pages/Login/Login';
+import Cadastro from './pages/cadastro/Cadastro';
+import Home from './pages/home/Home';
+import { AuthProvider } from './contexts/AuthContext';
+
 
 function App() {
   return (
     <>
-    <BrowserRouter>
-        <Navbar />
+    <AuthProvider>
+        <BrowserRouter>
+          <Navbar />
           <div className='min-h-[80vh]'>
             <Routes>
-              <Route path="/" element={<Home />} />
+              <Route path="/" element={<Login />} />
               <Route path="/login" element={<Login />} />
+              <Route path="/cadastro" element={<Cadastro />} />
               <Route path="/home" element={<Home />} />
             </Routes>
           </div>
           <Footer />
-    </BrowserRouter>
+        </BrowserRouter>
+        </AuthProvider>
     </>
-);
+  );
 }
 export default App;
